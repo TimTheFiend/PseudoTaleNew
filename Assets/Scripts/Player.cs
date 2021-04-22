11e700b665abfec33ec0fccff293a7144bb3f6c9
Joakim <tim@okoroko.com>
2021-04-22 13:20:55 +0200

    Cinemachine setup

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [Header("Player Variables")]
+    [Range(1f, 10f)]
     public float movementSpeed = 5f;
+    public string playerName = "Frisk";
+    public int lv = 1;
+    public int maxHP = 20;
+
     public StateMachine state = new StateMachine();
 
+    [Header("Runtime Variables")]
+    public int currentHP;
     [SerializeField]
     private List<GameObject> interactableEntities;  // TODO
+
 
     private void Awake() {
         interactableEntities = new List<GameObject>();
